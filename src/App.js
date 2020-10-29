629d124529253f919f692fa67935658c5c35721c
maksimus <gorlovmaks23@gmail.com>
2020-10-29 18:41:23 +0600

    Добавлен роутер для главной страницы

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Layout from "./components/Layout/Layout";
+import {Switch, Route} from "react-router-dom";
+import UrlForm from "./components/UrlForm/UrlForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={UrlForm}/>
+        <Route render={() => <h1>404 Not Found</h1>}/>
+      </Switch>
+    </Layout>
+);
 
 export default App;
